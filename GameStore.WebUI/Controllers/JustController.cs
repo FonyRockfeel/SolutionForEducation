563623eed720dc91c35df0e6c7eb470a469bf225
Rock <passhumster@gmail.com>
2018-03-02 18:34:10 +0300

    1

---
+++ GameStore.WebUI/Controllers/JustController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.WebUI.Models;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class JustController : Controller
+    {
+        // GET: Just
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RSVPForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            return View("Thanks", guest);
+        }
+    }
+}
